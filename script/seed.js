f944406c3b46fde60246770e9b795b6dcd96a97b
Chad Dayton <cdayton96@gmail.com>
2021-01-12 16:48:32 -0500

    fe: added to user store

--- script/seed.js
+++ script/seed.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict'
 
 const db = require('../server/db')
@@ -42,3 +43,64 @@ if (module === require.main) {
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
 module.exports = seed
+=======
+'use strict'
+
+const db = require('../server/db')
+const {User} = require('../server/db/models')
+const fs = require('fs')
+
+const parseCsv = csvData => {
+  const rows = csvData.split('\n')
+  const keys = rows.shift().split(',')
+  const parsedCsv = []
+  rows.forEach(row => {
+    const values = row.split(',')
+    const newObj = {}
+    keys.forEach((key, i) => {
+      newObj[key] = values[i]
+    })
+    parsedCsv.push(newObj)
+  })
+  return parsedCsv
+}
+
+const seed = async () => {
+  await db.sync({force: true})
+  console.log('db synced!')
+
+  try {
+    const userSeedCsv = fs.readFileSync('./userSeed.csv', 'utf-8')
+    const userSeedObjs = parseCsv(userSeedCsv)
+    const users = await User.bulkCreate(userSeedObjs)
+    console.log(`seeded ${users.length} users`)
+    console.log(`seeded successfully`)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function runSeed() {
+  console.log('seeding...')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  runSeed()
+}
+
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = seed
+>>>>>>> 902095a31ee4265e505ba74ea8efbedc4d8f6ea3
--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,21 @@
 const db = require('./db')
+const User = require('./models/user')
+const Product = require('./models/product')
+const {Sequelize} = require('sequelize/types')
 
-// register models
-require('./models')
+const Order = db.define('order', {
+  orderId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  quantity: {type: Sequelize.INTEGER, allowNull: false},
+  purchased: {type: Sequelize.BOOLEAN},
+  totalPrice: {type: Sequelize.FLOAT(2)},
+  date: {type: Sequelize.DATEONLY},
+})
+User.belongsToMany(Product, {through: 'Order'})
+Product.belongsToMany(User, {through: 'Order'})
 
-module.exports = db
+module.exports = {db, User, Product, Order}

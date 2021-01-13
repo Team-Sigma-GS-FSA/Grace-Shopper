1326c2da131c0b810762d9ba12f3132b6ff515ba
Chad Dayton <cdayton96@gmail.com>
2021-01-13 14:51:50 -0500

    FE: added GET SINGLE PRODUCT to reducer

--- client/store/product.js
+++ client/store/product.js
@@ -4,16 +4,19 @@ import {_deleteUser} from './user'
 //action types
 
 const GET_PRODUCTS = 'GET_PRODUCTS'
+const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
 const CREATE_PRODUCT = 'CREATE_PRODUCT'
 const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 const DELETE_PRODUCT = 'DELETE_PRODUCT'
 
 const productState = {
-  allProducts = [],
+  allProducts: [],
+  singleProduct: {},
 }
 
 //action creators
-const _getProducts = (product) => ({type: GET_PRODUCTS, product})
+const _getProducts = (products) => ({type: GET_PRODUCTS, products})
+const _getSingleProduct = (product) => ({type: GET_SINGLE_PRODUCT, product})
 const _createProduct = (product) => ({type: CREATE_PRODUCT, product})
 const _updateProduct = (product) => ({type: UPDATE_PRODUCT, product})
 const _deleteProduct = (product) => ({type: DELETE_PRODUCT, product})
@@ -22,6 +25,10 @@ const _deleteProduct = (product) => ({type: DELETE_PRODUCT, product})
 export const getProducts = () => async (dispatch) => {
   const {data} = await axios.get('/api/products')
   dispatch(_getProducts(data))
+}
+export const getSingleProduct = (product) => async (dispatch) => {
+  const {data} = await axios.get(`/api/products/${product.id}`)
+  dispatch(_getSingleProduct(data))
 }
 export const createProduct = (product) => async (dispatch) => {
   const {data} = await axios.post('/api/products', product)
@@ -36,19 +43,22 @@ export const deleteProduct = (product) => async (dispatch) => {
   dispatch(_deleteProduct(product))
 }
 
-
-export default function(state = productState, action){
-  switch(action.type){
+export default function (state = productState, action) {
+  switch (action.type) {
     case GET_PRODUCT:
-      return {...state, allProducts: action.product}
+      return {...state, allProducts: action.products}
+    case GET_SINGLE_PRODUCT:
+      return {...state, singleProduct: action.product}
     case CREATE_PRODUCT:
       return {...state, allProducts: [...allProducts, action.product]}
     case UPDATE_PRODUCT:
       return state.allProducts.map((product) =>
-        product.id ===action.product.id ? action.product : product
+        product.id === action.product.id ? action.product : product
       )
     case DELETE_PRODUCT:
-      return state.allProducts.filter((product) => product.id !== action.product.id)
+      return state.allProducts.filter(
+        (product) => product.id !== action.product.id
+      )
     default:
       return productState
   }

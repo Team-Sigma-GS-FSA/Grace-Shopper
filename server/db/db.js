0abf018f3716e8b1f89f4deb894e2145cbc6279c
Brad Gersh <bradley.gersh@gmail.com>
2021-01-16 14:08:33 -0500

    fix (modified db connection to work with heroku)

--- server/db/db.js
+++ server/db/db.js
@@ -7,7 +7,16 @@ const databaseName =
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    operatorAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: process.env.DATABASE_URL && {
+        require: true
+      }
+    }
   }
 );
 module.exports = db;
